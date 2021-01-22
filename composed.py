43c14c5851bbb0be7a846accb2e660de0ee31d02
james kee <43153742+jamesthekee@users.noreply.github.com>
2021-01-22 00:17:34 +0000

    doing some waccy stuff

---
+++ composed.py
@@ -0,0 +1,36 @@
+get_prime = (lambda a: list(filter(lambda b:b[0]==a, enumerate(list(filter(lambda c: all(c%i!=0 for i in range(2, int(c/2)+1)), range(2, 1337*2))))))[0][1])
+
+__import__("os").write(True, bytes((lambda _: _).__code__.co_name[....__str__()=="....":int(((0+1j)**(lambda _, __: _).__code__.co_argcount + True).real)].join((map(lambda x: chr(get_prime(get_prime(get_prime(0)))*int((get_prime(get_prime(0)**get_prime(0))*abs(x-pow(get_prime(1)*get_prime(get_prime(0)*get_prime(get_prime(0)-True)), 1/(get_prime(0)**get_prime(0)))+2)-x+pow(get_prime(get_prime(1)), 1/2)-2)/(2*abs(x-pow(get_prime(get_prime(get_prime(get_prime((get_prime(0)**get_prime(0))))*2)-1), 1/(get_prime(0)<<get_prime(0)))+2))) + ....__sizeof__()+True), range(get_prime(get_prime(0))*get_prime(0)*get_prime(0))))), "utf8"))
+
+five = get_prime(2)
+six = get_prime(0)*get_prime(get_prime(0)-True)
+four = (get_prime(0)**get_prime(0))
+eight = get_prime(0)<<get_prime(0)
+
+twenty = get_prime(get_prime(0))*get_prime(0)*get_prime(0)
+
+"""0 2
+1 3
+2 5
+3 7
+4 11
+5 13
+6 17
+7 19
+8 23
+9 29
+10 31
+11 37
+12 41
+13 43
+14 47
+15 53
+16 59
+17 61
+18 67
+19 71
+20 73
+21 79
+22 83
+23 89
+24 97"""
